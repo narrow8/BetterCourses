907bf145600f8235bac3b840588ba0c32c08a495
Robert Gogete <narrow8@hotmail.com>
2019-01-13 22:50:41 +0200

    asd

---
+++ BetterCourses/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BetterCourses
+{
+    class Person
+    {
+        public string faceId;
+        public FaceRectangle facerectangle;
+        public FaceAttributes faceAttributes;
+    }
+}
